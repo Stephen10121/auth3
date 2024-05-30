c39e75b5bca64ff94fe19981b68a6250fa650d66
Stephen Gruzin <stephengrzn@gmail.com>
2024-05-30 16:57:03 -0700

    Made the passkey card and logins card link to their respected pages

--- src/lib/components/mine/MostUsedPasskey.svelte
+++ src/lib/components/mine/MostUsedPasskey.svelte
@@ -22,13 +22,15 @@
     }
 </script>
 
-<Card.Root>
-    <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
-        <Card.Title class="text-sm font-medium">Most Used Passkey</Card.Title>
-        <KeyRound class="h-4 w-4 text-muted-foreground" />
-    </Card.Header>
-    <Card.Content>
-        <div class="text-2xl font-bold">{mostUsed.name}</div>
-        <p class="text-xs text-muted-foreground">Used {mostUsed.usage} time{mostUsed.usage !==1 ? "s" : ""}</p>
-    </Card.Content>
-</Card.Root>
+<a href="/settings/security">
+    <Card.Root>
+        <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+            <Card.Title class="text-sm font-medium">Most Used Passkey</Card.Title>
+            <KeyRound class="h-4 w-4 text-muted-foreground" />
+        </Card.Header>
+        <Card.Content>
+            <div class="text-2xl font-bold">{mostUsed.name}</div>
+            <p class="text-xs text-muted-foreground">Used {mostUsed.usage} time{mostUsed.usage !==1 ? "s" : ""}</p>
+        </Card.Content>
+    </Card.Root>
+</a>
--- src/lib/components/mine/TotalLoginsCard.svelte
+++ src/lib/components/mine/TotalLoginsCard.svelte
@@ -18,16 +18,15 @@
     }
 </script>
 
-
-<Card.Root>
-    <Card.Header
-        class="flex flex-row items-center justify-between space-y-0 pb-2"
-    >
-        <Card.Title class="text-sm font-medium">Total Logins</Card.Title>
-        <OpenDoor class="h-4 w-4 text-muted-foreground" />
-    </Card.Header>
-    <Card.Content>
-        <div class="text-2xl font-bold">{logins ? logins.length : "0"}</div>
-        <p class="text-xs text-muted-foreground">First login on {earliestLogin}</p>
-    </Card.Content>
-</Card.Root>
+<a href="/dashboard/login-history">
+    <Card.Root>
+        <Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+            <Card.Title class="text-sm font-medium">Total Logins</Card.Title>
+            <OpenDoor class="h-4 w-4 text-muted-foreground" />
+        </Card.Header>
+        <Card.Content>
+            <div class="text-2xl font-bold">{logins ? logins.length : "0"}</div>
+            <p class="text-xs text-muted-foreground">First login on {earliestLogin}</p>
+        </Card.Content>
+    </Card.Root>
+</a>
